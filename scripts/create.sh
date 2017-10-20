af2bee5885aa9ee8f0b2d0cf3961bb7b4555b34a
Derek <derek.schaefer@gmail.com>
2017-10-20 13:02:35 -0500

    configure the cert subject info to be even more generic

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 openssl req -x509 -nodes -sha256 -newkey rsa:2048 -days 3650 \
-  -subj '/C=US/ST=Illinois/L=Chicago/O=Example/OU=Example/CN=localhost' \
+  -subj '/C=US/ST=Example/L=Example/O=Example/OU=Example/CN=localhost' \
   -keyout $(pwd)/nginx/ssl.key -out $(pwd)/nginx/ssl.crt
